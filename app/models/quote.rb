485de62e32d2d46c9d3063bfb6970f2c98468b8f
Iuliia <julika27@gmail.com>
2019-05-31 17:49:48 +0000

    changed character limits for quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,4 @@
 class Quote < ApplicationRecord 
-  validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
-  validates :author, presence: true, length: { maximum: 50, minimum: 3 }
+  validates :saying, presence: true, length: { maximum: 280, minimum: 3 }
+  validates :author, presence: true, length: { maximum: 100, minimum: 3 }
 end
